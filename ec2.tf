6b9eb1f07c57675ff190f750d89da114226cae7d
justmorpheus <justdivyanshu@gmail.com>
2022-09-27 14:36:14 +0530

    update devsecops infrastructure as a code

--- ec2.tf
+++ ec2.tf
@@ -12,35 +12,38 @@ resource "aws_key_pair" "key121" {
   public_key = tls_private_key.oskey.public_key_openssh
 }
 
+variable "instance_names" {
+  default = ["devsecops-prod", "devsecops-jenkins",
+  "devsecops-vul_vault"]
+}
+
+variable "hostnames" {
+  default = ["script_prod.sh", "script_jenkins.sh",
+  "script_vm_valut.sh"]
+}
+
+data "template_file" "user-data" {
+  count    = "${length(var.hostnames)}"
+  template = "${file("${element(var.hostnames, count.index)}")}"
+}
+
+
 resource "aws_instance" "mytfinstance" {
+  count = 3
   ami           = "ami-017fecd1353bcc96e" #ubuntu 22
   instance_type = "t2.micro"
   key_name      = aws_key_pair.key121.key_name
   subnet_id = element(aws_subnet.prod-subnet-public-1.*.id, count.index)    
   vpc_security_group_ids = [aws_security_group.demo-devsecops.id]
+  user_data    = "${element(data.template_file.user-data.*.rendered, count.index)}"
+
   
-  count = 3
 
   tags = {
-    Name = "devsecops-prod-${count.index}"
+    Name = "${element(var.instance_names, count.index)}"
   }
 
-  provisioner "file" {
-    source      = "script.sh"
-    destination = "/tmp/script.sh"
-  }
 
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/script.sh",
-      "sudo /tmp/script.sh"
-    ]
-  }
 
-  connection {
-    type        = "ssh"
-    user        = var.INSTANCE_USERNAME
-    private_key = tls_private_key.oskey.private_key_pem
-    host        = aws_instance.mytfinstance[0].public_ip
-  }
+  
 }
